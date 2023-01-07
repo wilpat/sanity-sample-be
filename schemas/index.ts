f4c89442776ba53bb13056751991e31968f9ed81
William <wilpat456@gmail.com>
2023-01-07 20:02:03 +0000

    setup-backend

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import user from './user'
+import pin from './pin'
+import comment from './comment'
+import save from './save'
+import postedBy from './postedBy'
+
+export const schemaTypes = [user, pin, comment, save, postedBy]
